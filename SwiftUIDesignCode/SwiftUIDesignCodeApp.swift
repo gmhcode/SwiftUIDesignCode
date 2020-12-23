2f827671895cadfe83b24ce617d5b5a965cba9af
Greg Hughes <dcgg_101@hotmail.com>
2020-12-23 10:20:23 -0700

    Initial Commit

---
+++ SwiftUIDesignCode/SwiftUIDesignCodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIDesignCodeApp.swift
+//  SwiftUIDesignCode
+//
+//  Created by Greg Hughes on 12/23/20.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIDesignCodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
