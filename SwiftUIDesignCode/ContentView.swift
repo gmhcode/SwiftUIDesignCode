e08de812fddcd24a495a6d33340d158c949d8a5e
Greg Hughes <dcgg_101@hotmail.com>
2020-12-23 15:52:29 -0700

    added SelectCard animations

--- SwiftUIDesignCode/ContentView.swift
+++ SwiftUIDesignCode/ContentView.swift
@@ -10,45 +10,67 @@ import SwiftUI
 struct ContentView: View {
     @State var show = false
     @State var viewState = CGSize.zero
+    @State var showCard = false
+    
     var body: some View {
         ZStack {
             TitleView()
                 .blur(radius: show ? 20 : 0)
-                .animation(.default)
-            
+                .opacity(showCard ? 0.4 : 1)
+                .offset(y: showCard ? -200 : 0)
+                .animation(
+                    Animation
+                        .default
+                        .delay(0.1)
+//                        .speed(2)
+//                        .repeatCount(3, autoreverses: false)
+                )
+                
             
             BackCardView()
+                .frame(width: showCard ? 300 : 340, height: 220)
                 .background(show ? Color("card3") : Color("card4"))
                 .cornerRadius(20)
                 .shadow(radius: 20)
                 .offset(x: 0, y: show ?  -400 : -40)
                 .offset(x: viewState.width, y: viewState.height)
+                .offset(y: showCard ? -180 : 0)
                 //scale is making the views APPEAR as different sizes, but in reality they are the same size
-                .scaleEffect(0.9)
+                .scaleEffect(showCard ? 1 : 0.9)
                 .rotationEffect(.degrees(show ? 0 : 10))
-                .rotation3DEffect(Angle(degrees: 10), axis: (x: 10.0, y: 0, z: 0.0))
+                .rotationEffect(Angle(degrees: showCard ? -10 : 0))
+                .rotation3DEffect(Angle(degrees: showCard ? 0 : 10), axis: (x: 10.0, y: 0, z: 0.0))
                 .blendMode(.hardLight)
                 .animation(.easeInOut(duration: 0.5))
             
             BackCardView()
+                .frame(width: 340, height: 220)
                 .background(show ? Color("card4") : Color("card3"))
                 .cornerRadius(20)
                 .shadow(radius: 20)
                 .offset(x: 0, y: show ?  -200 : -20)
                 .offset(x: viewState.width, y: viewState.height)
-                .scaleEffect(0.95)
+                .offset(y: showCard ? -140 : 0)
+                .scaleEffect(showCard ? 1 : 0.95)
                 .rotationEffect(Angle(degrees: show ? 0 : 5))
-                .rotation3DEffect(Angle(degrees: 5), axis: (x: 10.0, y: 0, z: 0.0))
+                .rotationEffect(Angle(degrees: showCard ? -5 : 0))
+                .rotation3DEffect(Angle(degrees:  showCard ? 0 : 5), axis: (x: 10.0, y: 0, z: 0.0))
                 .blendMode(.hardLight)
                 .animation(.easeInOut(duration: 0.3))
             
             CardView()
+                .frame(width: showCard ? 375 : 340.0, height: 220)
+                .background(Color.black)
+//                .cornerRadius(20)
+                .clipShape(RoundedRectangle(cornerRadius: 20, style: .continuous))
+                .shadow(radius: 20)
                 //This offset is the thing that actuall moves the view
                 .offset(x: viewState.width, y: viewState.height)
+                .offset(y: showCard ? -100 : 0)
                 .animation(.spring(response: 0.3, dampingFraction: 1, blendDuration: 0))
                 .blendMode(.hardLight)
                 .onTapGesture {
-                    self.show.toggle()
+                    self.showCard.toggle()
                 }
                 .gesture(
                     //This tells the view where its going to be moved to, when it is moved
@@ -63,8 +85,9 @@ struct ContentView: View {
             
             
             BottomCardView()
+                .offset(x: 0, y: showCard ? 360 : 1000)
                 .blur(radius: show ? 20 : 0)
-                .animation(.default)
+                .animation(.timingCurve(0.2, 0.8, 0.2, 1, duration: 0.8))
         }
     }
 }
@@ -98,10 +121,7 @@ struct CardView: View {
                 .aspectRatio(contentMode: .fill)
                 .frame(width: 300, height: 110, alignment: .top)
         }
-        .frame(width: 340.0, height: 220)
-        .background(Color.black)
-        .cornerRadius(20)
-        .shadow(radius: 20)
+        
     }
 }
 
@@ -109,7 +129,7 @@ struct BackCardView: View {
     var body: some View {
         VStack {
             Spacer()
-        }.frame(width: 340, height: 220)
+        }
         
         
     }
@@ -150,6 +170,5 @@ struct BottomCardView: View {
         .background(Color.white)
         .cornerRadius(30)
         .shadow(radius: 20)
-        .offset(x: 0, y: 500)
     }
 }
