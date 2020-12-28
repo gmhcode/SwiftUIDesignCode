322dd789cf5cc33517826aa89eeb9ee97b4e0e7d
Greg Hughes <dcgg_101@hotmail.com>
2020-12-28 15:23:36 -0700

    Extracted HomeView from Home.swift

--- SwiftUIDesignCode/Home.swift
+++ SwiftUIDesignCode/Home.swift
@@ -15,48 +15,29 @@ struct Home: View {
         ZStack {
             Color(#colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1))
                 .edgesIgnoringSafeArea(.all)
-            VStack {
-                HStack {
-                    Text("Watching")
-                        .font(.system(size: 28, weight: .bold))
-                    
-                    Spacer()
-                    
-                    Button(action: {self.showProfile.toggle()}) {
-                        Image("Avatar")
-                            .resizable()
-                            .frame(width: 36, height: 36)
-                            .clipShape(Circle())
-                    }
-                }
-                .padding(.horizontal)
+            HomeView(showProfile: $showProfile)
+                .padding(.top, 44)
                 
-                .padding(.top,30)
+                .background(Color.white)
                 
-                Spacer()
-            }
-            .padding(.top, 44)
-            
-            .background(Color.white)
-            
-            .clipShape(RoundedRectangle(cornerRadius: 30, style: .continuous))
-            
-            .shadow(color: Color.black.opacity(0.2), radius: 20, x: 0, y: 20)
-            
-            .offset(y: showProfile ? -450 : 0)
-            
-            .rotation3DEffect(Angle(degrees: showProfile ? Double(viewState.height / 10) - 10 : 0), axis: (x: 10.0, y: 0, z: 0))
-            
-            //shrinks the Home View when showProfile = true
-            .scaleEffect(showProfile ? 0.9 : 1)
-            
-            .animation(.spring(response: 0.5, dampingFraction: 0.6, blendDuration: 0))
-            
-            .edgesIgnoringSafeArea(.all)
+                .clipShape(RoundedRectangle(cornerRadius: 30, style: .continuous))
+                
+                .shadow(color: Color.black.opacity(0.2), radius: 20, x: 0, y: 20)
+                
+                .offset(y: showProfile ? -450 : 0)
+                
+                .rotation3DEffect(Angle(degrees: showProfile ? Double(viewState.height / 10) - 10 : 0), axis: (x: 10.0, y: 0, z: 0))
+                
+                //shrinks the Home View when showProfile = true
+                .scaleEffect(showProfile ? 0.9 : 1)
+                
+                .animation(.spring(response: 0.5, dampingFraction: 0.6, blendDuration: 0))
+                
+                .edgesIgnoringSafeArea(.all)
             
             MenuView()
                 .background(Color.black.opacity(0.001))
-                .offset(y: showProfile ? 0 : 600)
+                .offset(y: showProfile ? 0 : screen.height)
                 .offset(y: viewState.height)
                 .animation(.spring(response: 0.5, dampingFraction: 0.6, blendDuration: 0))
                 .onTapGesture {
@@ -84,3 +65,18 @@ struct Home_Previews: PreviewProvider {
         Home()
     }
 }
+
+struct AvatarView: View {
+    @Binding var showProfile: Bool
+    
+    var body: some View {
+        Button(action: {self.showProfile.toggle()}) {
+            Image("Avatar")
+                .resizable()
+                .frame(width: 36, height: 36)
+                .clipShape(Circle())
+        }
+    }
+}
+
+let screen = UIScreen.main.bounds
---
+++ SwiftUIDesignCode/HomeView.swift
@@ -0,0 +1,36 @@
+//
+//  HomeView.swift
+//  SwiftUIDesignCode
+//
+//  Created by Greg Hughes on 12/28/20.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    @Binding var showProfile : Bool
+    var body: some View {
+        VStack {
+            HStack {
+                Text("Watching")
+                    .font(.system(size: 28, weight: .bold))
+                
+                Spacer()
+                
+                AvatarView(showProfile: $showProfile)
+            }
+            .padding(.horizontal)
+            
+            .padding(.top,30)
+            
+            Spacer()
+        }
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        
+        HomeView(showProfile: .constant(false))
+    }
+}
