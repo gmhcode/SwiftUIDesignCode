5f58e86ee5e5365a04e4ca5c8ea1834ff113280c
Greg Hughes <dcgg_101@hotmail.com>
2020-12-28 17:37:21 -0700

    Created the UpdateDetail Screen

---
+++ SwiftUIDesignCode/UpdateDetail.swift
@@ -0,0 +1,33 @@
+//
+//  UpdateDetail.swift
+//  SwiftUIDesignCode
+//
+//  Created by Greg Hughes on 12/28/20.
+//
+
+import SwiftUI
+
+struct UpdateDetail: View {
+    
+    var update: Update = updateData[1]
+    
+    var body: some View {
+        List {
+            VStack {
+                Image(update.image)
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(maxWidth:  .infinity)
+                Text(update.text)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+            }
+            .navigationBarTitle(update.title)
+        }.listStyle(DefaultListStyle())
+    }
+}
+
+struct UpdateDetail_Previews: PreviewProvider {
+    static var previews: some View {
+        UpdateDetail()
+    }
+}
