c2bc232ae1a31df217665013ec3a36b4a2656057
Greg Hughes <dcgg_101@hotmail.com>
2021-01-12 16:52:29 -0700

    implemented login image animation

--- SwiftUIDesignCode/LoginView.swift
+++ SwiftUIDesignCode/LoginView.swift
@@ -8,6 +8,11 @@
 import SwiftUI
 
 struct LoginView: View {
+    
+    @State var show = false
+    @State var viewState = CGSize.zero
+    @State var isDragging = false
+    
     var body: some View {
         ZStack(alignment: .top) {
             Color.black.edgesIgnoringSafeArea(.all)
@@ -28,11 +33,14 @@ struct LoginView: View {
                 }
                 .frame(maxWidth: 375, maxHeight: 120)
                 .padding(.horizontal,16)
+                //allows the text to move around with the background
+                .offset(x: viewState.width/15, y: viewState.height/15)
                 
                 Text("80 hours of courses for SwiftUI, React and design tools")
                     .font(.subheadline)
                     .frame(width: 250)
-                    
+                    //allows the text to move around with the background
+                    .offset(x: viewState.width/20, y: viewState.height/20)
                 Spacer()
                     
             }
@@ -44,17 +52,47 @@ struct LoginView: View {
                 ZStack {
                     Image(uiImage: #imageLiteral(resourceName: "Blob"))
                         .offset(x:-150,y:-200)
+                        .rotationEffect(Angle(degrees: show ? 360+90 : 90))
                         .blendMode(.plusDarker)
+                        .animation(Animation.linear(duration: 120).repeatForever(autoreverses: false))
+                        .onAppear{
+                            self.show = true
+                        }
                     Image(uiImage: #imageLiteral(resourceName: "Blob"))
                         .offset(x:-200,y:-250)
+                        .rotationEffect(Angle(degrees: show ? 360 : 0), anchor: .leading)
                         .blendMode(.overlay)
+                        .animation(Animation.linear(duration: 100).repeatForever(autoreverses: false))
                 }
             )
-            .background(Image(uiImage: #imageLiteral(resourceName: "Card3")),alignment: .bottom)
+            .background(
+                Image(uiImage: #imageLiteral(resourceName: "Card3"))
+                    //allows the image to move with the background
+                    .offset(x: viewState.width/25, y: viewState.height/25),
+                alignment: .bottom
+            )
             .background(Color(#colorLiteral(red: 0.4117647059, green: 0.4705882353, blue: 0.9725490196, alpha: 1)))
             .clipShape(RoundedRectangle(cornerRadius: 30, style: .continuous))
+            //makes the 3d effect smooth
+            .scaleEffect(isDragging ? 0.9 : 1)
+            .animation(.timingCurve(0.2, 0.8, 0.2, 1, duration: 0.8))
+            //3d effect
+            .rotation3DEffect(
+                Angle(degrees: 5),
+                axis: (x: viewState.width, y: viewState.height, z: 0.0)
+                )
+            .gesture(
+                DragGesture().onChanged { value in
+                    self.viewState = value.translation
+                    self.isDragging = true
+                }
+                .onEnded{ value in
+                    self.viewState = .zero
+                    self.isDragging = false
+                    
+                }
+            )
         }
-        
     }
 }
 
