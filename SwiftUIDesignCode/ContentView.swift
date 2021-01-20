2cdef79582d1879c25892710383f78d5b0d3e869
Greg Hughes <dcgg_101@hotmail.com>
2021-01-20 15:02:25 -0700

    implemented geometry reader on contentView and HomeView so they now adapt to all screen sizes

--- SwiftUIDesignCode/ContentView.swift
+++ SwiftUIDesignCode/ContentView.swift
@@ -34,7 +34,8 @@ struct ContentView: View {
             
             
             BackCardView()
-                .frame(width: showCard ? 300 : 340, height: 220)
+                .frame(maxWidth: showCard ? 300 : 340)
+                .frame(height: 220)
                 .background(show ? Color("card3") : Color("card4"))
                 .cornerRadius(20)
                 .shadow(radius: 20)
@@ -50,7 +51,8 @@ struct ContentView: View {
                 .animation(.easeInOut(duration: 0.5))
             
             BackCardView()
-                .frame(width: 340, height: 220)
+                .frame(maxWidth: 340)
+                .frame(height: 220)
                 .background(show ? Color("card4") : Color("card3"))
                 .cornerRadius(20)
                 .shadow(radius: 20)
@@ -65,7 +67,8 @@ struct ContentView: View {
                 .animation(.easeInOut(duration: 0.3))
             
             CardView()
-                .frame(width: showCard ? 375 : 340.0, height: 220)
+                .frame(maxWidth: showCard ? 375 : 340.0)
+                .frame(height: 220)
                 .background(Color.black)
                 //                .cornerRadius(20)
                 .clipShape(RoundedRectangle(cornerRadius: 20, style: .continuous))
@@ -91,35 +94,38 @@ struct ContentView: View {
             
 //            Text("\(bottomState.height)").offset(y: -300)
             
-            BottomCardView(show: $showCard)
-                .offset(x: 0, y: showCard ? 360 : 1000)
-                .offset(y: bottomState.height)
-                .blur(radius: show ? 20 : 0)
-                .animation(.timingCurve(0.2, 0.8, 0.2, 1, duration: 0.8))
-                .gesture(
-                    DragGesture().onChanged { value in
-                        self.bottomState = value.translation
-                        if self.showFull {
-                            self.bottomState.height += -300
+            GeometryReader { bounds in
+                BottomCardView(show: $showCard)
+                    .offset(x: 0, y: showCard ? bounds.size.height / 2 : bounds.size.height)
+                    .offset(y: bottomState.height + bounds.safeAreaInsets.top + bounds.safeAreaInsets.bottom)
+                    .blur(radius: show ? 20 : 0)
+                    .animation(.timingCurve(0.2, 0.8, 0.2, 1, duration: 0.8))
+                    .gesture(
+                        DragGesture().onChanged { value in
+                            self.bottomState = value.translation
+                            if self.showFull {
+                                self.bottomState.height += -300
+                            }
+                            if self.bottomState.height < -300 {
+                                self.bottomState.height = -300
+                            }
+                            
                         }
-                        if self.bottomState.height < -300 {
-                            self.bottomState.height = -300
+                        .onEnded { value in
+                            if self.bottomState.height > 50 {
+                                self.showCard = false
+                            }
+                            if (self.bottomState.height < -100 && !self.showFull) || (self.bottomState.height < -250 && self.showFull) {
+                                self.bottomState.height = -300
+                                self.showFull = true
+                            } else {
+                                self.bottomState = .zero
+                                self.showFull = false
+                            }
                         }
-                        
-                    }
-                    .onEnded { value in
-                        if self.bottomState.height > 50 {
-                            self.showCard = false
-                        }
-                        if (self.bottomState.height < -100 && !self.showFull) || (self.bottomState.height < -250 && self.showFull) {
-                            self.bottomState.height = -300
-                            self.showFull = true
-                        } else {
-                            self.bottomState = .zero
-                            self.showFull = false
-                        }
-                    }
                 )
+            }
+//            .edgesIgnoringSafeArea(.all)
         }
     }
 }
@@ -127,6 +133,7 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
             ContentView()
+                .previewLayout(.fixed(width: 320, height: 667))
     }
 }
 
@@ -178,6 +185,9 @@ struct TitleView: View {
             }
             .padding()
             Image("Background1")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(maxWidth: 375)
             Spacer()
         }
     }
@@ -217,9 +227,10 @@ struct BottomCardView: View {
         }
         .padding(.top, 8)
         .padding(.horizontal, 20)
-        .frame(maxWidth: .infinity)
+        .frame(maxWidth: 712)
         .background(BlurView(style: .systemThinMaterial))
         .cornerRadius(30)
         .shadow(radius: 20)
+        .frame(maxWidth: .infinity )
     }
 }
